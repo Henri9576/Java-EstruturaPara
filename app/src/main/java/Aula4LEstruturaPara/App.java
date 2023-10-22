1cffdec02e069f95dc8a554108f20e5721ab06ea
Henrique9576 <henrique_costa02@outlook.com>
2023-10-22 17:31:59 -0300

    Initial commit

---
+++ app/src/main/java/Aula4LEstruturaPara/App.java
@@ -0,0 +1,11 @@
+package Aula4LEstruturaPara;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        for(int i = 0; i <=10; i = i + 1){
+            System.out.println("Este código foi repetido mais uma vez.");
+        }
+    }
+}
